c941ef3d1b365344becd8d6f584196d2f5b03ccf
Chemod-77 <42216966+21D4N404@users.noreply.github.com>
2019-01-16 13:10:58 +0700

    Update visit.sh

--- visit.sh
+++ visit.sh
@@ -1,15 +1,18 @@
-r='\033[31m'
-y='\033[33m'
-b='\033[34m'
-c='\033[36m'
-w='\033[0m'
+green="\033[32;1m"
+yellow="\033[33;1m"
+indigo="\033[34;1m"
+ungu="\033[35;1m"
+purple="\033[37;1m"
+cyan="\033[36;1m"
+white="\033[39;1m"
+red="\033[31;1m"
 
 input()
 {
 
 #InputData
 read -p "Input URL : " link
-read -p "Jumlah	  : " count
+read -p "Jumlah	   : " count
 
 #URL_Autovisitor
 url=$(curl -s -X POST 'https://javaghost.net/ini/visitor.php?' -d 'dav_site='${link}'&razin_jml='${count}'&submit=Visit' | grep "<tr><br><a><font color='#fff'>" | cut -d ":" -f2)
@@ -24,8 +27,8 @@ $e
 for i in $url
 do
 
-	echo -e "[$b+$w] visitor dengan IP : $y$i$w sukses berkunjung:*"
-	sleep 10s #Biar aman hehe
+	echo "$yellow IP Pengunjung$red  ⋙$cyan $i $red ⋙ $green Done!"
+	sleep 1s #Biar aman hehe
 
 done
 }
